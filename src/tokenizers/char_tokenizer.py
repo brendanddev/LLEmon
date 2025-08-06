e9655d70d3a5661cf70ac379e691e15da956993b
Brendan Dileo <dileob23@gmail.com>
2025-08-06 17:50:54 -0400

    Create char tokenizer

---
+++ src/tokenizers/char_tokenizer.py
@@ -0,0 +1,12 @@
+
+""" 
+char_tokenizer.py
+Defines a character level tokenizer for text processing
+
+Brendan Dileo, August 2025
+"""
+
+from src.utils.text_processing import load_text, count_characters
+
+
+class CharTokenizer:
