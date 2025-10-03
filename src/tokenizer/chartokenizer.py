196b2a964b5bd6e0bbf8f1c89c77dc97651423a5
Brendan Dileo <dileob23@gmail.com>
2025-10-03 09:24:10 -0400

    Setup char tokenizer for tokenizing characters

--- src/tokenizer/chartokenizer.py
+++ src/tokenizer/chartokenizer.py
@@ -2,4 +2,18 @@
 """ 
 chartokenizer.py
 
+A simple character-level tokenizer.
 """
+
+import torch
+
+class CharTokenizer:
+    
+    def __init__(self, text):
+        # Sorted set of unique characters in the text
+        self.chars = sorted(list(set(text)))
+        self.vocab_size = len(self.chars)
+        
+        # Mappings from char to index and index to char
+        self.stoi = {ch: i for i, ch in enumerate(self.chars)}
+        self.itos = {i: ch for i, ch in enumerate(self.chars)}
