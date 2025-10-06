7a0a99ad39de082808dad9367988bd72515cf51d
Brendan Dileo <dileob23@gmail.com>
2025-10-06 15:36:36 -0400

    Create test script

---
+++ src/test.py
@@ -0,0 +1,9 @@
+
+""" 
+test.py
+
+Simple test script to test individual components of the LLEmon system.
+"""
+
+from tokenizer.chartokenizer import CharTokenizer
+from models.transformer import Transformer
