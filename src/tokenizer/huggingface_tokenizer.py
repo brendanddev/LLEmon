0d2841c8079b8691b01d8869edfaa567d0ec958a
Brendan Dileo <dileob23@gmail.com>
2025-10-14 16:31:29 -0400

    Setup class

---
+++ src/tokenizer/huggingface_tokenizer.py
@@ -0,0 +1,21 @@
+
+""" 
+huggingface_tokenizer.py
+
+"""
+
+from tokenizers import Tokenizer, models, trainers, pre_tokenizers
+
+class HuggingFaceTokenizer:
+    
+    def __init__(self, vocab_path):
+        pass
+    
+    def train():
+        pass
+    
+    def encode(self, text):
+        pass
+    
+    def decode(self, tokens):
+        pass
