35672700b5cadc4aad538c001c76b09b8ad0ac27
Brendan Dileo <dileob23@gmail.com>
2025-10-14 16:33:32 -0400

    Add init method

--- src/tokenizer/huggingface_tokenizer.py
+++ src/tokenizer/huggingface_tokenizer.py
@@ -7,13 +7,20 @@ huggingface_tokenizer.py
 from tokenizers import Tokenizer, models, trainers, pre_tokenizers
 
 class HuggingFaceTokenizer:
-    
+
+    # Initializes the tokenizer, loading from vocab_path if provided
     def __init__(self, vocab_path):
-        pass
+        if vocab_path:
+            self.tokenizer = Tokenizer.from_file(vocab_path)
+        else:
+            self.tokenizer = Tokenizer(models.BPE())
+            self.tokenizer.pre_tokenizer = pre_tokenizers.Whitespace()
     
-    def train():
-        pass
-    
+        self.vocab_size = self.tokenizer.get_vocab_size()
+        
+    def train(self, files, vocab_size=30000):
+        
+        
     def encode(self, text):
         pass
     
